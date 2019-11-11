81547caac872e7bb8d73d2aee6bf625f8f641fe7
wangchaoyong <1357773401@qq.com>
2019-11-11 14:46:04 +0800

    局部变量、成员变量、静态变量定义原则

--- src/main/java/com/wcy/ssm/util/DateConverter.java
+++ src/main/java/com/wcy/ssm/util/DateConverter.java
@@ -8,6 +8,15 @@ import java.util.Date;
 
 public class DateConverter implements Converter<String, Date> {
     //private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh-mm-ss");
+    /*
+        SimpleDateFormat定义为局部变量还是成员变量还是静态变量？
+        1.对象是否线程安全？
+            线程不安全，定义局部变量
+        2.对象所属外部对象是单例还是多例的？
+            单例对象：定义成员变量、静态变量均可
+            多例对象：定义静态变量
+
+    */
     @Override
     public Date convert(String source) {
         Date date = null;
